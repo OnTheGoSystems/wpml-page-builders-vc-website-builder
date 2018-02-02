7e5243cf6fed93aef6f49e38ba4b5410ee2fe90f
Adriano Ferreira <adriano.f@onthegosystems.com>
2018-02-02 15:10:19 -0200

    [wpmlcore-5170] Created WPML_VC_Website_Builder_Integration_Factory

---
+++ tests/phpunit/tests/test-wpml-vc-website-builder-integration-factory.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Class Test_WPML_VC_Website_Builder_Integration_Factory
+ *
+ * @runTestsInSeparateProcesses
+ * @preserveGlobalState disabled
+ *
+ * @group page-builders
+ * @group vc-website-builder
+ * @group wpmlcore-5165
+ */
+class Test_WPML_VC_Website_Builder_Integration_Factory extends OTGS_TestCase {
+
+	/**
+	 * @test
+	 */
+	public function it_creates() {
+		global $sitepress;
+
+		$bb_factory = new WPML_VC_Website_Builder_Integration_Factory();
+
+		$absolute_links              = \Mockery::mock( 'overload:AbsoluteLinks' );
+		$absolute_to_permalinks      = \Mockery::mock( 'overload:WPML_Absolute_To_Permalinks' );
+		$translate_link_targets      = \Mockery::mock( 'overload:WPML_Translate_Link_Targets' );
+		$string_registration         = \Mockery::mock( 'overload:WPML_PB_String_Registration' );
+		$string_registration_factory = \Mockery::mock( 'overload:WPML_String_Registration_Factory' );
+		$hooks_strategy              = \Mockery::mock( 'overload:WPML_PB_API_Hooks_Strategy' );
+		$string_factory              = \Mockery::mock( 'overload:WPML_ST_String_Factory' );
+
+		$string_registration_factory->shouldReceive( 'create' )->andReturn( $string_registration );
+
+		$sitepress = $this->getMockBuilder( 'SitePress' )
+		                  ->setMethods( array( 'get_active_languages' ) )
+		                  ->disableOriginalConstructor()
+		                  ->getMock();
+
+		$bb_factory->create();
+
+		$this->assertInstanceOf( 'WPML_Page_Builders_Integration', $bb_factory->create() );
+	}
+}
